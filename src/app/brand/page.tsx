9c393eae46c0a70010040afca0847d159414c556
basseyriman <basseyriman@gmail.com>
2026-08-03 22:19:52 +0100

    Update brand flyer to use user uploaded model image

--- src/app/brand/page.tsx
+++ src/app/brand/page.tsx
@@ -45,7 +45,7 @@ export default function BrandFlyersPage() {
             <div className="absolute -right-64 -bottom-12 w-[450px] aspect-[3/4] bg-white p-4 shadow-2xl rotate-[12deg] rounded-lg z-30">
               <div className="w-full h-full bg-gray-100 rounded-sm overflow-hidden pb-0">
                 <img 
-                  src="https://images.unsplash.com/photo-1524504388940-b1c1722653e1?w=800&q=80" 
+                  src="/model-outfit.jpg" 
                   alt="Fashion Model" 
                   className="w-full h-full object-cover"
                 />
