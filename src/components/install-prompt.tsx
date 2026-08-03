07784c426ff99a6b2162be1ab9f81feb854a5eb9
basseyriman <basseyriman@gmail.com>
2026-08-03 16:25:08 +0100

    Smooth out install prompt text

--- src/components/install-prompt.tsx
+++ src/components/install-prompt.tsx
@@ -54,8 +54,9 @@ export function InstallPrompt() {
           <p className="text-mist text-xs leading-relaxed">
             Install this app to your phone in seconds. Tap{" "}
             <Share className="inline-block h-4 w-4 align-sub text-blue-500" />{" "}
-            (or the three dots <span className="font-bold">...</span> then <Share className="inline-block h-4 w-4 align-sub text-blue-500" />) below, then select{" "}
-            <span className="font-medium text-ivory">"Add to Home Screen"</span>{" "}
+            (or tap <span className="font-bold">...</span> then Share{" "}
+            <Share className="inline-block h-4 w-4 align-sub text-blue-500" />),{" "}
+            then select <span className="font-medium text-ivory">"Add to Home Screen"</span>{" "}
             <PlusSquare className="inline-block h-4 w-4 align-sub text-ivory" />.
           </p>
         </div>
